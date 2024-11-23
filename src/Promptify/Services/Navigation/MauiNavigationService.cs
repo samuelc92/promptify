6f29e8b4f185a363012134647c7238b8d6962ac3
Samuel Couto <samuel.couto92@gmail.com>
2024-11-24 00:29:27 +0100

    Create MauiNavigationService.cs

---
+++ src/Promptify/Services/Navigation/MauiNavigationService.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Promptify.Services.Navigation;
+
+public class MauiNavigationService : INavigationService
+{
+    public Task InitializeAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task NavigateToAsync(string route, IDictionary<string, object> routeParameters = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task PopAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
