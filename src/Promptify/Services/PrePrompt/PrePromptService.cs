67f47ba4bb8749ae7bade71569174157bd191f7d
Samuel Couto <samuel.couto92@gmail.com>
2024-11-25 21:29:11 +0100

    Create PrePromptService.cs

---
+++ src/Promptify/Services/PrePrompt/PrePromptService.cs
@@ -0,0 +1,8 @@
+﻿using Promptify.Models;
+
+namespace Promptify.Services.PrePrompt;
+
+public class PrePromptService : IPrePromptService
+{
+    public IEnumerable<Prompt> Prompts { get; set; } = [];
+}
