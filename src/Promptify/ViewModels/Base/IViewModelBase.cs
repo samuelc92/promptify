22398a333ffee92688a28efce4b0bf6de5269270
Samuel Couto <samuel.couto92@gmail.com>
2024-11-25 20:55:05 +0100

    Update IViewModelBase.cs

--- src/Promptify/ViewModels/Base/IViewModelBase.cs
+++ src/Promptify/ViewModels/Base/IViewModelBase.cs
@@ -1,5 +1,15 @@
 ﻿namespace Promptify.ViewModels.Base;
 
+using CommunityToolkit.Mvvm.Input;
+using Services.Navigation;
+
 public interface IViewModelBase : IQueryAttributable
 {
+    public INavigationService NavigationService { get; }
+
+    public IAsyncRelayCommand InitializeAsyncCommand { get; }
+
+    public bool IsBusy { get; }
+
+    Task InitializeAsync();
 }
