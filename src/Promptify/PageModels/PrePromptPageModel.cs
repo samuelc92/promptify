c3a00114889fe89a489324b20732c6704e58c25a
Samuel Couto <samuel.couto92@gmail.com>
2024-11-25 20:54:41 +0100

    Create PrePromptPageModel.cs

---
+++ src/Promptify/PageModels/PrePromptPageModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Promptify.PageModels;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using Models;
+
+public partial class PrePromptPageModel : ObservableObject
+{
+    [ObservableProperty]
+    private List<Prompt> _prompts = [];
+}
