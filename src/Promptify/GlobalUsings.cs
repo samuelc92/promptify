59c743f3d9b4d5d01e012c5af06d36babecfa1f6
Samuel Couto <samuel.couto92@gmail.com>
2024-11-26 21:35:11 +0100

    Update GlobalUsings.cs

--- src/Promptify/GlobalUsings.cs
+++ src/Promptify/GlobalUsings.cs
@@ -1,6 +1,8 @@
-global using Fonts;
 global using Promptify.Data;
 global using Promptify.PageModels;
 global using Promptify.Pages;
 global using Promptify.Services;
 global using Promptify.Utilities;
+global using CommunityToolkit.Mvvm.ComponentModel;
+global using CommunityToolkit.Mvvm.Input;
+global using Fonts;
