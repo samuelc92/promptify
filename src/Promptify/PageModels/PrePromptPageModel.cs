70134bc196d9d278c1f22d38a2c12d7d3494e021
Samuel Couto <samuel.couto92@gmail.com>
2024-11-26 21:35:43 +0100

    Update PrePromptPageModel.cs

--- src/Promptify/PageModels/PrePromptPageModel.cs
+++ src/Promptify/PageModels/PrePromptPageModel.cs
@@ -1,10 +1,17 @@
 ﻿namespace Promptify.PageModels;
 
-using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Models;
+using System.Collections.Generic;
 
 public partial class PrePromptPageModel : ObservableObject
 {
+    [ObservableProperty]
+    private string _description = string.Empty;
+
+    [ObservableProperty]
+    private string _text = string.Empty;
+
     [ObservableProperty]
     private List<Prompt> _prompts = [];
 }
