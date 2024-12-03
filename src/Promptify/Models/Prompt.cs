aba963b6be245d78afd0c1123f02a768dfc57e72
Samuel Couto <samuel.couto92@gmail.com>
2024-12-03 23:12:12 +0100

    Update Prompt.cs

--- src/Promptify/Models/Prompt.cs
+++ src/Promptify/Models/Prompt.cs
@@ -2,6 +2,8 @@
 
 public class Prompt : BindableObject
 {
+    public int Id { get; set; }
+
     public string Description{ get; set; } = string.Empty;
 
     public string Text { get; set; } = string.Empty;
